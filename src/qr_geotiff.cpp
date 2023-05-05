f1774dc02a8bcb6705ac06016ae1de1ec9726ba0
Sumikama <s.sumikama.914@nitech.jp>
2023-05-06 05:00:32 +0900

    geotiffにqrを埋め込むために必要なpkg作った

---
+++ src/qr_geotiff.cpp
@@ -0,0 +1,36 @@
+#include <ros/ros.h>
+#include <geometry_msgs/TransformStamped.h>
+#include "imax_qrcode/TransformStampedArray.h"
+#include "hector_worldmodel_msgs/PosePercept.h"
+#include "hector_worldmodel_msgs/PerceptInfo.h"
+
+
+ros::Publisher pub;
+void tfsaCb(const imax_qrcode::TransformStampedArrayConstPtr& msg)
+{
+  hector_worldmodel_msgs::PosePercept pp;
+  for (int i = 0; i < msg->transformStampedArray.size(); i++){
+    pp.header.frame_id = msg->transformStampedArray[i].header.frame_id;
+    pp.header.stamp = msg->transformStampedArray[i].header.stamp;
+    pp.pose.pose.position.x = msg->transformStampedArray[i].transform.translation.x;
+    pp.pose.pose.position.y = msg->transformStampedArray[i].transform.translation.y;
+    pp.pose.pose.position.z = msg->transformStampedArray[i].transform.translation.z;
+    pp.info.class_id = "qrcode";
+    pp.info.class_support = 1.0;
+    pp.info.object_support = 1.0;
+    pp.info.name = msg->transformStampedArray[i].child_frame_id;
+    pub.publish(pp);
+  }
+  
+}
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "qr_geotiff");
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("tfsa", 10, tfsaCb);
+  pub = nh.advertise<hector_worldmodel_msgs::PosePercept>("/worldmodel/pose_percept", 10);
+
+  ros::spin();
+  return 0;
+}
