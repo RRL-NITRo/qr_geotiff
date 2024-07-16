9b8ff7a608d15a667cdcf53eae801ff5efa84894
kotarokanazawa <clp13214@nitech.jp>
2024-07-17 01:38:12 +0900

    object用

--- src/qr_geotiff.cpp
+++ src/qr_geotiff.cpp
@@ -39,8 +39,25 @@ void tfhazmatCb(const imax_qrcode::TransformStampedArrayConstPtr& msg)
     pp.info.name = msg->transformStampedArray[i].child_frame_id;
     pub.publish(pp);
   }
-  
 }
+
+void tfobjectCb(const imax_qrcode::TransformStampedArrayConstPtr& msg)
+{
+  hector_worldmodel_msgs::PosePercept pp;
+  for (int i = 0; i < msg->transformStampedArray.size(); i++){
+    pp.header.frame_id = msg->transformStampedArray[i].header.frame_id;
+    pp.header.stamp = msg->transformStampedArray[i].header.stamp;
+    pp.pose.pose.position.x = msg->transformStampedArray[i].transform.translation.x;
+    pp.pose.pose.position.y = msg->transformStampedArray[i].transform.translation.y;
+    pp.pose.pose.position.z = msg->transformStampedArray[i].transform.translation.z;
+    pp.info.class_id = msg->transformStampedArray[i].child_frame_id;
+    pp.info.class_support = 1.0;
+    pp.info.object_support = 1.0;
+    pp.info.name = msg->transformStampedArray[i].child_frame_id;
+    pub.publish(pp);
+  }
+}
+
 
 
 int main(int argc, char** argv)
@@ -49,6 +66,7 @@ int main(int argc, char** argv)
   ros::NodeHandle nh;
   ros::Subscriber sub = nh.subscribe("/tfsa", 10, tfsaCb);
   ros::Subscriber sub_hazmat = nh.subscribe("/tf_hazmat", 10, tfhazmatCb);
+   ros::Subscriber sub_object = nh.subscribe("/tf_object", 10, tfobjectCb);
   pub = nh.advertise<hector_worldmodel_msgs::PosePercept>("/worldmodel/pose_percept", 10);
 
   ros::spin();
